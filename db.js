e87c5007968aba79e4dc0b3a86992619931c3873
Pandi Selvi <sithanathanpandiselvi@gmail.com>
2022-03-06 15:44:08 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+var mongoDBURL = 'mongodb+srv://customer:12345@cluster0.h0sbj.mongodb.net/test'
+
+mongoose.connect(mongoDBURL , {useUnifiedTopology:true , useNewUrlParser:true})
+
+var dbconnect = mongoose.connection
+
+dbconnect.on('error' , ()=>{
+    console.log(`Mongo DB Connection Failed`);
+})
+
+dbconnect.on('connected' , ()=>{
+    console.log(`Mongo DB Connection Successfull`);
+})
+
+module.exports = mongoose
